84db4085650cf48ab32d0fb6992fd56d227b0b15
Maximolucentini <ms.lucentini@alumno.um.edu.ar>
2024-08-15 19:09:16 -0300

    Finalizando las posiciones iniciales de todas las piezas

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,18 @@
 ## Changelog
 
-## [0.1] - 13 / 08 / 2024
+## [0.1] - 15/08/2024
 
 ## Agregado
+
+- Terminando las posiciones iniciales de todas las piezas
 
 ## [0.1] - 13/08/2024
 
 ## Agregado
 
 - Iniciando la clase Board
+
+## [0.1] - 13 / 08 / 2024
+
+## Agregado
+
--- board.py
+++ board.py
@@ -1,32 +1,50 @@
-from pawn import Pawn
 from rook import Rook
 from knight import Knight
-
-
+from bishop import Bishop
+from queen import Queen
+from king import King
+from pawn import Pawn
 class Board:
     def __init__(self):
-        self.grid = [[None for _ in range(8)] for _ in range(8)]
+        self.__positions__ = [[None for _ in range(8)] for _ in range(8)]
         self.setup_board()
 
     def setup_board(self):
         
         for col in range(8):
-            self.grid[1][col] = Pawn("BLACK", f"{col+1}", position=(1, col))
-            self.grid[6][col] = Pawn("WHITE", f"{col+1}", position=(6, col))
+            self.__positions__[1][col] = Pawn("BLACK", f"{col+1}", position=(1, col))
+            self.__positions__[6][col] = Pawn("WHITE", f"{col+1}", position=(6, col))
 
         
-        self.grid[0][0] = Rook("BLACK", position=(0, 0))
-        self.grid[0][7] = Rook("BLACK", position=(0, 7))
-        self.grid[7][0] = Rook("WHITE", position=(7, 0))
-        self.grid[7][7] = Rook("WHITE", position=(7, 7))
+        self.__positions__[0][0] = Rook("BLACK")
+        self.__positions__[0][7] = Rook("BLACK")
+        self.__positions__[7][0] = Rook("WHITE")
+        self.__positions__[7][7] = Rook("WHITE")
 
         
-        self.grid[0][1] = Knight("BLACK", position=(0, 1))
-        self.grid[0][6] = Knight("BLACK", position=(0, 6))
-        self.grid[7][1] = Knight("WHITE", position=(7, 1))
-        self.grid[7][6] = Knight("WHITE", position=(7, 6))
+        self.__positions__[0][1] = Knight("BLACK")
+        self.__positions__[0][6] = Knight("BLACK")
+        self.__positions__[7][1] = Knight("WHITE")
+        self.__positions__[7][6] = Knight("WHITE")
+
         
+        self.__positions__[0][2] = Bishop("BLACK")
+        self.__positions__[0][5] = Bishop("BLACK")
+        self.__positions__[7][2] = Bishop("WHITE")
+        self.__positions__[7][5] = Bishop("WHITE")
+
+        
+        self.__positions__[0][3] = Queen("BLACK")
+        self.__positions__[7][3] = Queen("WHITE")
+
+        
+        self.__positions__[0][4] = King("BLACK")
+        self.__positions__[7][4] = King("WHITE")
+
     def get_piece(self, row, col):
-        return self.__positions__[row][col]    
+        
+        return self.__positions__[row][col]
+
+   
 
  
